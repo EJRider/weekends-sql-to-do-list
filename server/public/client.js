1fa2c3f98800a4ae4c3470f04013b7395b2ffa9e
Rider <goejrider@gmail.com>
2022-10-06 19:12:55 -0500

    Add markTaskDone and deleteTask, tested and finished

--- server/public/client.js
+++ server/public/client.js
@@ -5,25 +5,112 @@ $(document).ready(onReady);
 function onReady(){
     console.log('in onReady');
     $('#addBtn').on('click', addTask);
+    $('#viewTasks').on('click','.dltBtn', dltTask);
+    $('#viewTasks').on('click', '.mrkDoneBtn', markTaskDone);
     getTask();
 };
 
 function addTask(){
     console.log('in addTask');
-}
+    let newTask = {
+        taskName: $('#taskNameIn').val(),
+        task: $('#taskIn').val(),
+        taskDone: false
+    }
+    $.ajax({
+        url: '/to-do',
+        method: 'POST',
+        data: newTask
+    })
+        .then(response=>{
+            console.log('sent task');
+            cleanUp();
+            getTask();
+        })
+        .catch(err=>{
+            console.log('error in addTask', err);
+        });
+};
 
 function getTask(){
     console.log('in getTask');
-}
+    $.ajax({
+        url: '/to-do',
+        method: 'GET'
+    })
+        .then(response=>{
+            console.log('getting tasks');
+            render(response);
+        })
+        .catch(err=>{
+            console.log('error in getTask', err);
+        });
+};
 
 function dltTask(){
-    console.log('in dltTask');
+    console.log('in dltTask', $(this).data('id'));
+    let taskId = $(this).data('id');
+    $.ajax({
+        url:`/to-do/${taskId}`,
+        method: 'DELETE'
+    })
+        .then(response=>{
+            console.log('deleting task')
+            getTask();
+        })
+        .catch(err=>{
+            console.log('error in dltTask', err);
+        })
 }
 
 function markTaskDone(){
-    console.log('in markTaskDone');
-}
+    console.log('in markTaskDone', $(this).data('id'));
+    let taskId = $(this).data('id');
+    $.ajax({
+        url: `/to-do/${taskId}`,
+        method: 'PUT'
+    })
+        .then(response=>{
+            console.log('marking task done');
+            getTask();
+        })
+        .catch(err=>{
+            console.log('error in MTD', err);
+        });
+};
 
-function render(){
+function cleanUp(){
+    $('#taskNameIn').val('');
+    $('#taskIn').val('');
+};
+
+function render(tasks){
     console.log('in render');
-}
+    $('#viewTasks').empty()
+    for(let task of tasks){
+        if(task.taskDone === true){
+            $('#viewTasks').append(`
+                <tr>
+                    <td>${task.taskName}</td>
+                    <td>${task.task}</td>
+                    <td>✅</td>
+                    <td> <button data-id='${task.id}' class="dltBtn">Delete Task</button> </td>
+                </tr>
+            `)
+        }
+        else{
+            $('#viewTasks').append(`
+            <tr>
+                <td>${task.id}</td>
+                <td>${task.taskName}</td>
+                <td>${task.task}</td>
+                <td>❌</td>
+                <td> 
+                <button data-id='${task.id}' class="mrkDoneBtn">Mark Task Done</button>
+                <button data-id='${task.id}' class="dltBtn">Delete Task</button> 
+                </td>
+            </tr>
+        `)
+        }     
+    }
+};
